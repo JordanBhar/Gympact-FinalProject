4d69f62462b5dd339e8273be8d6364cfe435e74a
Jordan Bhar <bharjor@sheridancollege.ca>
2022-11-21 17:38:42 -0500

    Initial Commit

---
+++ Gympact/GympactApp.swift
@@ -0,0 +1,17 @@
+//
+//  GympactApp.swift
+//  Gympact
+//
+//  Created by Jordan Bhar on 2022-11-21.
+//
+
+import SwiftUI
+
+@main
+struct GympactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
