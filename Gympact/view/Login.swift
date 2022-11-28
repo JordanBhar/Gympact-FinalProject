ff3e6e272825e1724878734cc9d79cea4102a59b
Muaz_A9 <50706598+ahmadmua@users.noreply.github.com>
2022-11-28 11:25:02 -0500

    UpdatedProj2

---
+++ Gympact/view/Login.swift
@@ -0,0 +1,116 @@
+//
+//  ScreenFive.swift
+//  SigninV2
+//
+//  Created by Гералд Бирген on 16.12.2020.
+//
+
+import SwiftUI
+import Firebase
+import FirebaseAuth
+
+    struct Login: View {
+        
+        @State var email: String = "Test@gmail.com"
+        @State var password: String = "123456"
+        @State private var selection: Int? = nil
+        @State private var userLoggedIn = false
+        @State private var showingAlert = false
+        @State private var msg = ""
+        
+        var body: some View {
+            
+                VStack(spacing: 30){
+                    
+                    NavigationLink(destination: Register(), tag: 1, selection: self.$selection){}
+                    
+                    NavigationLink(destination: Information(), tag: 2, selection: self.$selection){}
+                    
+                    
+                    
+                    Spacer()
+                    
+                    Text("LOGIN")
+                        .fontWeight(.bold)
+                        .font(.largeTitle)
+                        .foregroundColor(Color.blue)
+                        .padding(.bottom, 100)
+                    
+                    
+                    // Form
+                    VStack(spacing: 25){
+                        TextField("Email", text: $email)
+                            .padding(10)
+                            .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.black, lineWidth: 0.5).frame(height: 45))
+                            .multilineTextAlignment(.center)
+                        
+                        VStack(spacing:10){
+                            SecureField("Password", text: $password)
+                                .padding(10)
+                                .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.black, lineWidth: 0.5).frame(height: 45))
+                                .multilineTextAlignment(.center)
+                            
+                        }
+                    }
+                    
+                    // SignIn
+                    Button(action: {
+                        login()
+                    }){
+                        Text("Sign In")
+                            .modifier(CustomTextM(fontName: "MavenPro-Bold", fontSize: 16, fontColor: Color.white))
+                        
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 56, alignment: .leading)
+                            .background(Color.blue)
+                            .cornerRadius(10)
+                    }
+                    .alert(self.msg, isPresented: $showingAlert) {
+                                Button("OK", role: .cancel) { }
+                            }
+                    
+                    Spacer()
+                    
+                    // SignUp
+                    VStack(spacing: 10){
+                        Text("Don't have an account?")
+                            .modifier(CustomTextM(fontName: "Oxygen-Regular", fontSize: 18, fontColor: Color.primary))
+                        Button(action: {
+                            self.selection = 1
+                        }){
+                            Text("Register")
+                                .modifier(CustomTextM(fontName: "Oxygen-Bold", fontSize: 18, fontColor: Color.blue))
+                        }
+                    }
+                }
+                .padding(.horizontal,30)
+                .padding(.vertical, 25)
+                .onAppear {
+                    Auth.auth().addStateDidChangeListener{auth, user in
+                        if user != nil {
+                            userLoggedIn.toggle()
+                        }
+                    }
+                }
+        }
+        
+        func login(){
+            Auth.auth().signIn(withEmail: email, password: password){result, error in
+                if error != nil {
+                    showingAlert = true
+                    msg = error!.localizedDescription
+                } else {
+                    self.selection = 2
+                }
+            }
+        }
+    }
+    
+    
+    struct Login_Previews: PreviewProvider {
+        static var previews: some View {
+            Login()
+        }
+    }
+    
+
