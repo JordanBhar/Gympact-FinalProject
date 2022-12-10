30bc274a244aa891ef3dc071fe05dfea7afd467e
Muaz_A9 <50706598+ahmadmua@users.noreply.github.com>
2022-12-10 03:41:30 -0500

    Workout plan - work in progress

--- Gympact/view/WorkoutPlan.swift
+++ Gympact/view/WorkoutPlan.swift
@@ -7,10 +7,50 @@
 
 import SwiftUI
 
+struct Bookmark: Identifiable {
+
+    
+    let id = UUID()
+    let name: String
+    var items: [Bookmark]?
+    
+
+    static let back_Monday = Bookmark(name: "Back & Biceps")
+   
+    static let monday = Bookmark(name: "Monday", items: [Bookmark.back_Monday])
+    static let tuesday = Bookmark(name: "Tuesday", items: [])
+    static let wednesday = Bookmark(name: "Wednesday",  items: [])
+    static let thursday = Bookmark(name: "Thursday",  items: [])
+    static let friday = Bookmark(name: "Friday",  items: [])
+}
+
+
 struct WorkoutPlan: View {
+    
+    let items: [Bookmark] = [.monday, .tuesday, .wednesday, .thursday, .friday]
+
+
+    
     var body: some View {
-        Text("WORKOUT PLAN")
+        
+        VStack{
+            
+            Text("Workout Plan")
+                .fontWeight(.bold)
+                .font(.largeTitle)
+                .foregroundColor(Color.blue)
+                .padding(.top, 40)
+            
+            List(items, children: \.items) { row in
+                HStack {
+                    Text(row.name)
+                        .font(.title2)
+                }
+            }
+        }
     }
+    
+
 }
 
 struct WorkoutPlan_Previews: PreviewProvider {
