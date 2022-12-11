e073969803cc035dc2a7f00b931a7bb54af3ff9c
Muaz_A9 <50706598+ahmadmua@users.noreply.github.com>
2022-12-10 22:56:52 -0500

    workout plan update

--- Gympact/view/WorkoutPlan.swift
+++ Gympact/view/WorkoutPlan.swift
@@ -6,54 +6,65 @@
 //
 
 import SwiftUI
-
-struct Bookmark: Identifiable {
-
-    
-    let id = UUID()
-    let name: String
-    var items: [Bookmark]?
-    
-
-    static let back = Bookmark(name: "Back & Biceps")
-   
-    static let monday = Bookmark(name: "Monday", items: [
-    
-    ])
-    static let tuesday = Bookmark(name: "Tuesday", items: [])
-    static let wednesday = Bookmark(name: "Wednesday",  items: [])
-    static let thursday = Bookmark(name: "Thursday",  items: [])
-    static let friday = Bookmark(name: "Friday",  items: [])
-}
+import Firebase
+import FirebaseAuth
 
 
 struct WorkoutPlan: View {
     
-    let items: [Bookmark] = [.monday, .tuesday, .wednesday, .thursday, .friday]
-
-
+    @State var item = ["Biceps", "Triceps"]
+    @State var isExpanded = false
     
+
     var body: some View {
         
         VStack{
             
-            Text("Workout Plan")
-                .fontWeight(.bold)
-                .font(.largeTitle)
-                .foregroundColor(Color.blue)
-                .padding(.top, 40)
-            
-            List(items, children: \.items) { row in
-                HStack {
-                    Text(row.name)
-                        .font(.title2)
-                }
+            List {
+                DisclosureGroup(
+                    content: {
+                        ForEach(item, id: \.self){ item in
+                            Text("\(item)")
+                        }
+                    },
+                    label: {Text("Monday")})
+                
+                DisclosureGroup(
+                    content: {
+                        Text("Tuesday")
+                        
+                    },
+                    label: {Text("Tuesday")})
+                
+                DisclosureGroup(
+                    content: {
+                        Text("Wednesday")
+                        
+                    },
+                    label: {Text("Wednesday")})
+                
+                DisclosureGroup(
+                    content: {
+                        Text("Thursday")
+                        
+                    },
+                    label: {Text("Thursday")})
+                
+                DisclosureGroup(
+                    content: {
+                        Text("Friday")
+                        
+                    },
+                    label: {Text("Friday")})
             }
+            
         }
+        
     }
     
 
 }
+
 
 struct WorkoutPlan_Previews: PreviewProvider {
     static var previews: some View {
